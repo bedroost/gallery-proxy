82831ba8bb3c3dc2e58654b0beb0d27c0a8aa32b
Joel <joelkaplan1@gmail.com>
2019-07-17 16:15:47 -0700

    add deploy branch

--- public/index.js
+++ public/index.js
@@ -6,4 +6,16 @@ const port = process.env.PORT || 3000;
 
 app.use('/rooms/:listingid', express.static(path.join(__dirname)));
 
+app.get('/api/:listingid/images', (req, res) => {
+
+});
+
+app.get('/api/:listingid/reviews', (req, res) => {
+
+});
+
+app.get('/api/:listingid/booking', (req, res) => {
+
+});
+
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
